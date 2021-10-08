27b2741ccc712cf8d4bd988378fcb5cb2e1a6c26
Alejandro Largo <largoalejo@gmail.com>
2021-10-08 17:52:27 -0500

    watch, useBeforeRouteLeave

--- src/views/Pokemon.vue
+++ src/views/Pokemon.vue
@@ -1,16 +1,45 @@
 <template>
-  <h1>Pokemon View</h1>
+  <h1 v-if="!pokemon && !errorMessage">Buscando...</h1>
+  <h1 v-else-if="errorMessage">{{ errorMessage }}</h1>
+
+  <template v-else>
+    <h3>{{ pokemon.name }}</h3>
+    <img :src="pokemon.sprites.front_default" alt="pokemon.name" />
+    <br />
+    <router-link :to="{ name: 'pokemon-search' }">Regresar</router-link>
+  </template>
 </template>
 
 <script>
 // useRoute para manejo de la ruta
-import { useRoute } from "vue-router";
+import { watch } from "vue";
+import { useRoute, onBeforeRouteLeave } from "vue-router";
+import usePokemon from "../composables/usePokemon";
 
 export default {
   setup() {
     const route = useRoute();
+    console.log(route.params.id);
+    const { pokemon, isLoading, errorMessage, searchPokemon } = usePokemon(
+      route.params.id
+    );
 
-    console.log(route.params);
+    watch(
+      () => route.params.id,
+      (/* value, prevValue */) => searchPokemon(route.params.id)
+    );
+
+    onBeforeRouteLeave(() => {
+      const answer = window.confirm("¿Esta seguro que desea salir?");
+
+      if (!answer) return false;
+    });
+
+    return {
+      errorMessage,
+      isLoading,
+      pokemon,
+    };
   },
 };
 </script>
